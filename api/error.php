cec771f2c98d9dd0a4e9c98dc5150e15a8db5c9d
InduttanzaCibernetica <dottorpadrinrandazzo@gmail.com>
2026-05-21 17:39:13 +0200

    Update error.php

--- api/error.php
+++ api/error.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Invia una risposta di errore in formato JSON e termina lo script.
- *
- * @param string $message   Messaggio di errore.
- * @param int    $code      Codice HTTP (default 400).
- */
+
 function sendError($message, $code = 400)
 {
     http_response_code($code);
